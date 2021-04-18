a9e4af601a74080dc9a1e1fa89eb2350bd60312d
omar azaz <cool_omer@hotmail.co.uk>
2021-04-18 17:31:08 +0500

    wip

--- BMA-R30X.h
+++ BMA-R30X.h
@@ -151,6 +151,188 @@ class BMA{
             }
             return true;
         }
+
+        uint8_t receivePacket(uint32_t timeout = DEFAULT_TIMEOUT, bool print_data = false){
+            uint8_t confirm_code = 0xFF;
+            
+            uint8_t* data_buffer = new uint8_t[64]; // data buffer must be 64 bytes
+            uint8_t serial_buffer[FPS_DEFAULT_SERIAL_BUFFER_LENGTH] = {0}; // will store high byte at start of array
+            uint16_t serial_buffer_length = 0;
+            uint8_t byte_buffer = 0;
+
+            // wait for response for set timout
+            while(timeout > 0){
+                if(commSerial->available()){
+                    byte_buffer = commSerial->read();
+                    
+                    if(print_data) Serial.println(byte_buffer, HEX);
+
+                    serial_buffer[serial_buffer_length] = byte_buffer;
+                    serial_buffer_length++;
+                }
+
+                timeout--;
+                delay(1);
+            }
+
+            if(serial_buffer_length == 0){
+                Serial.println("received no response\n");
+                return false;
+            }
+
+            if(serial_buffer_length < 10){
+                Serial.print("bad packet nigga\n");
+                return false;
+            }
+
+            uint16_t token = 0;   // to iterate over switch cases
+            uint8_t packet_type;  // received packet type to use in checking checksum
+            uint8_t rx_packet_length[2]; // received packet length
+            uint16_t rx_packet_length_2b; // single data type for holding received packet length
+            uint16_t data_length; // length of data extracted from packet length
+            uint8_t confirmation_code = 0xFF;
+            uint8_t checksum_bytes[2]; // checksum in bytes
+            uint16_t checksum; // whole checksum representation
+
+            //the following loop checks each segment of the data packet for errors
+            while(1){
+                switch(token){
+                // case 0 & 1 check for header
+                case 0:
+                    // high byte
+                    if(serial_buffer[token] == ((HEADER >> 8) & 0xFF)) break;
+                    
+                    return false;
+                    
+                case 1:
+                    // low byte
+                    if(serial_buffer[token] == (HEADER & 0xFF)) break;
+                    
+                    return false;
+                    
+                // cases 2 to 5 check for device address. high to low byte
+                case 2:
+                    if(serial_buffer[token] == ((M_ADDRESS >> 24) & 0xFF) ) break;
+                    
+                    return false;
+
+                case 3:
+                    if(serial_buffer[token] == ((M_ADDRESS >> 16) & 0xFF) ) break;
+                    
+                    return false;
+
+                case 4:
+                    if(serial_buffer[token] == ((M_ADDRESS >> 8) & 0xFF) ) break;
+                    
+                    return false;
+
+                case 5:
+                    if(serial_buffer[token] == (M_ADDRESS & 0xFF) ) break;
+                    
+                    return false;
+
+                // check for valid packet type
+                case 6:
+                    if((serial_buffer[token] == PID_DATA) || (serial_buffer[token] == PID_ACKNOWLEDGE) || (serial_buffer[token] == PID_DATA_END)) {
+                        packet_type = serial_buffer[token];
+                        break;
+                    }
+                    
+                    return false;
+
+                // read packet data length
+                case 7:
+                    if((serial_buffer[token] > 0) || (serial_buffer[token + 1] > 0)){
+                        rx_packet_length[0] = serial_buffer[token + 1]; // low byte
+                        rx_packet_length[1] = serial_buffer[token]; // high byte
+                        rx_packet_length_2b = uint16_t(rx_packet_length[1] << 8) + rx_packet_length[0]; // the full length
+                        data_length = rx_packet_length_2b - 3; // 2 checksum , 1 conformation code
+                        
+                        token ++;
+
+                        break;
+                    }
+                    
+                    return false;
+
+                // case 8 won't be hit as after case 7, token value is 9
+                // read conformation code
+                case 9:
+                    confirmation_code = serial_buffer[token];
+                    break;
+
+                // read data
+                case 10:
+                    for(int i = 0; i < data_length; i++){
+                        data_buffer[(data_length - 1) - i] = serial_buffer[token + i]; // low bytes will be written at end of array. Inherently, high bytes appear first
+                    }
+                    break;
+
+                // read checksum
+                case 11:
+                    // if there ain't no data
+                    if(data_length == 0){
+                        checksum_bytes[0] = serial_buffer[token]; // low byte
+                        checksum_bytes[1] = serial_buffer[token - 1]; // high byte
+                        checksum = uint16_t(checksum_bytes[1] << 8) + checksum_bytes[0];
+
+                        uint16_t tmp = 0;
+
+                        tmp = packet_type + rx_packet_length[0] + rx_packet_length[1] + confirmation_code;
+
+                        // if calculated checksum is equal to received checksum. is good to go
+                        if(tmp == checksum){
+                            if(print_data) Serial.println("no data pass");
+                            return confirmation_code;
+                        }
+                        if(print_data) Serial.println("no data fail");
+                        return confirmation_code;
+
+                        break;
+                    }
+
+                    // if data is present
+                    else if((serial_buffer[token + (data_length -1)] > 0) || ((serial_buffer[token + 1 + (data_length -1)] > 0))){
+                        checksum_bytes[0] = serial_buffer[token + 1 + (data_length -1 )]; // low byte
+                        checksum_bytes[1] = serial_buffer[token + (data_length -1 )]; // high byte {changed here}
+                        checksum = uint16_t (checksum_bytes[1] << 8) + checksum_bytes[0];
+
+                        uint16_t tmp = 0;
+
+                        tmp = packet_type + rx_packet_length[0] + rx_packet_length[1] + confirmation_code;
+                        rx_data = new uint8_t[data_length];
+                        rx_data_length = data_length;
+                        
+                        for(int i=0; i < data_length; i++) {
+                            tmp += data_buffer[i]; //calculate data checksum
+                            rx_data[i] = data_buffer[i]; // get data to be used later
+                        }
+
+                        if(tmp == checksum) {
+                            if(print_data) Serial.println("data present pass");
+                            return confirmation_code;
+                        }
+
+                        if(print_data) Serial.println("data present fail");
+                        return confirmation_code;
+
+                        break;
+                    }
+
+                    // if checksum is 0
+                    else {
+                        if(print_data) Serial.println("checksum 0");
+                        return confirmation_code;
+                    }
+
+                default:
+                    Serial.println("default");
+                    return confirmation_code;
+                }
+
+                token ++;
+            }
+        }
 };
 
 #endif
